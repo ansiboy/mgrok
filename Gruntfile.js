47655cd1ae3ee154276cdd7bd57eb8f6b9e0a19a
maishu <ansiboy@163.com>
2018-01-30 09:14:32 +0800

    add make

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,38 +4,44 @@
 
 const path = require("path");
 var cert_name = '*.mgrok.cn';
-// var build_tag = 'debug'
+
 module.exports = function (grunt) {
 
-    let is_win = /^win/.test(process.platform);
+
+    let is_win = /^win/.test(process.platform);         
+
 
     let build_tag = grunt.option('build');
-    if (build_tag != 'debug' && build_tag != 'release') {
+    if(build_tag!='debug' && build_tag != 'release') {
         console.log(`
 usage:
 grunt --build=debug
 grunt --build=release
         `);
-        return;
     }
 
     let working_path = path.resolve("./");
-    let set_gopath_command = is_win ? `SET GOPATH=${working_path}` : `GOPATH=${working_path}`;
-
-    console.log(set_gopath_command);
-
+    let set_gopath_command = setEnvVariableCommand("GOPATH",working_path);
+    let output_path = `bin/${build_tag}`;
+    
     grunt.initConfig({
         copy: {
             cert: {
-                files: [
+                files: build_tag == 'debug' ?
+                [
+                    { expand: true, cwd: `cert/${cert_name}_debug/`, src: `ngrokroot.crt`, dest: 'assets/client/tls/' },
+                    { expand: true, cwd: `cert/${cert_name}_debug/`, src: `snakeoil.crt`, dest: `assets/server/tls/` },
+                    { expand: true, cwd: `cert/${cert_name}_debug/`, src: `snakeoil.key`, dest: `assets/server/tls/` }
+                ] :
+                [
                     { expand: true, cwd: `cert/${cert_name}/`, src: `ngrokroot.crt`, dest: 'assets/client/tls/' },
                     { expand: true, cwd: `cert/${cert_name}/`, src: `snakeoil.crt`, dest: `assets/server/tls/` },
                     { expand: true, cwd: `cert/${cert_name}/`, src: `snakeoil.key`, dest: `assets/server/tls/` }
-                ]
+                ] 
             },
             ngrok_config: {
                 files: [
-                    { expand: true, cwd: `src/ngrok/main/ngrok/`, src: '.ngrok', dest: 'bin' }
+                    { expand: true, cwd: `src/ngrok/main/ngrok/`, src: '.ngrok', dest: output_path }
                 ]
             }
         },
@@ -43,31 +49,42 @@ grunt --build=release
             options: {
                 stderr: true
             },
-            build_client: {
-                command: `
-                    ${set_gopath_command}
-                    go-bindata -nomemcopy -pkg=assets -tags=${build_tag} -debug=${build_tag == 'debug' ? 'true' : 'false'} -o=src/ngrok/client/assets/assets_${build_tag}.go assets/client/...
-                    go build -o bin/ngrok -tags "${build_tag}"  src/ngrok/main/ngrok/ngrok.go
-                `
-            },
-            build_server: {
-                command: `
-                    ${set_gopath_command}
-                    go-bindata -nomemcopy -pkg=assets -tags=${build_tag} -debug=${build_tag == 'debug' ? 'true' : 'false'} -o=src/ngrok/server/assets/assets_${build_tag}.go assets/server/...
-                    go build -o bin/ngrokd -tags "${build_tag}"  src/ngrok/main/ngrokd/ngrokd.go
-                `
-            },
-            build_ngrok: {
+            build: {
                 command: [
-                    set_gopath_command,
+                    setEnvVariableCommand("GOPATH", working_path),
+                    `go-bindata -nomemcopy -pkg=assets -tags=${build_tag} -debug=${build_tag == 'debug' ? 'true' : 'false'} -o=src/ngrok/client/assets/assets_${build_tag}.go assets/client/...`,
+                    `go build -o ${output_path}/ngrok${is_win?'.exe':''} -tags "${build_tag}"  src/ngrok/main/ngrok/ngrok.go`,
                     `go-bindata -nomemcopy -pkg=assets -tags=${build_tag} -debug=${build_tag == 'debug' ? 'true' : 'false'} -o=src/ngrok/server/assets/assets_${build_tag}.go assets/server/...`,
-                    `go build -o bin/ngrok -tags "${build_tag}"  src/ngrok/main/ngrok/ngrok.go`
+                    `go build -o ${output_path}/ngrokd${is_win?'.exe':''} -tags "${build_tag}"  src/ngrok/main/ngrokd/ngrokd.go`
+                    
+                ].join("&&")
+            },
+            all_client:{
+                command: [
+                    `sudo GOOS=linux GOARCH=amd64 make release-client`,
+                    `sudo GOOS=linux GOARCH=386 make release-client`,
+                    `sudo GOOS=linux GOARCH=arm make release-client`,
+                    `sudo GOOS=windows GOARCH=amd64 make release-client`,
+                    `sudo GOOS=windows GOARCH=386 make release-client`,
+                    `sudo GOOS=darwin GOARCH=amd64 make release-client`,
+                    `sudo GOOS=darwin GOARCH=386 make release-client`,
                 ].join('&&')
             }
         }
     });
 
+    
+    function setEnvVariableCommand(name, value){
+        return is_win ? `SET ${name}=${value}` : `${name}=${value}` ;
+     }
+
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-shell');
-    grunt.registerTask('default', ['copy', 'shell']);
+
+    let tasks = ['copy', 'shell:build'];
+    if(build_tag == 'release') {
+        tasks.push('shell:all_client')
+    }
+    grunt.registerTask('default', tasks);
 }
+
